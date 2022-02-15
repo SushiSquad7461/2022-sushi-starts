c1c68af63d6b221e131fd093e0ddb2cd0be299fe
Daniel Gorbatov <dg20@uw.edu>
2022-02-14 16:08:11 -0800

    heroku test

--- notion.js
+++ notion.js
@@ -100,7 +100,7 @@ export async function markPresent(tag) {
 
     console.log(time);
 
-    if( ( (day > 0 && day < 6) && (time < 16) ) || (day === 6 && time < 11)) {
+    if( ( (day > 0 && day < 6) && (time < 17) ) || (day === 6 && time < 11)) {
       pageId = currPage.results[0].id;
       let people = currPage.results[0].properties.Attendees.people;
       people.push(user);

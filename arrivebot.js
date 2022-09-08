9059fee3e7b0c542703bb53e85cd50e00cb9ad65
Kevin Tran <me@kevintran.codes>
2022-09-08 16:50:47 -0700

    refactor arrival bot

--- arrivebot.js
+++ arrivebot.js
@@ -4,7 +4,7 @@ import { markPresent, getAttendees, logPing} from "./notion.js";
 let client;
 let guild;
 
-export default function createArrivalBot(curr_attendees) {
+export default function createArrivalBot(curAttendees) {
     client = new Client({
         intents: [
             Intents.FLAGS.GUILDS,
@@ -13,37 +13,37 @@ export default function createArrivalBot(curr_attendees) {
     });
 
     client.on('ready', () => {
-          console.log(`Logged in as ${client.user.tag}!`);
+        console.log(`Logged in as ${client.user.tag}!`);
     });
 
     client.on('messageCreate', async (message) => {
-        if (message.mentions.has(client.user) && message.author !== client.user) {
-            console.log("User is arriving");
-            console.log(message.author.username + "#" + message.author.discriminator);
-
+        if (message.mentions.has(client.user) && message.author !== client.user && message.author.username === "KTOmega") {
+            const user = `${message.author.username}#${message.author.discriminator}`;
             const date = new Date(message.createdTimestamp);
 
-            message.reply("Welcome " + message.author.username);
+            let ping = curAttendees.attendees_id.map(id => `<@${id}>`).join(" ");
+
+            console.log(`User arriving: ${user}`);
             await markPresent(message.author.username + "#" + message.author.discriminator, date);
 
-            let ping = curr_attendees.attendees_id.map(id => `<@${id}>`).join(" ");
-
             if (ping === "") {
-                ping = "No current attendees";
+                ping = `Welcome, \`${message.author.username}\`. There is no one here yet.`;
             } else {
-                const messageContentWithoutPing = message.content.replace(`<@!${client.user.id}>`, "");
+                const messageContentWithoutPing = message.content.replace(`<@${client.user.id}>`, "").trim();
 
                 // keep message content first so people see that first on their phone notifications
                 ping = `${messageContentWithoutPing} ${ping}`;
             }
 
-            if (message.content.includes("door")) {
+            if (message.content.includes("door") || message.content.includes("inner")) {
                 message.reply(ping);
+            } else {
+                message.reply(`Welcome, \`${message.author.username}\`. If you need to be let in, please specify which door you're at.`);
             }
 
-            if (!curr_attendees.findAttendee(message.author.id)) {
-                await logPing(false, message.author.username + "#" + message.author.discriminator);
-                curr_attendees.addAttendee(message.author.username + "#" + message.author.discriminator, message.author.id);
+            if (!curAttendees.findAttendee(message.author.id)) {
+                await logPing(false, user);
+                curAttendees.addAttendee(user, message.author.id);
             }
         }
     });
